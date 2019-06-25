f9104173473804883dc6c808587f57b3398410e1
lmoreno127 <luismorenor127@hotmail.com>
2019-06-25 14:48:27 -0500

    Add nil test for stuff in box

--- box_specs.rb
+++ box_specs.rb
@@ -1,6 +1,17 @@
 describe Box do
-    let(:box) {Box.new}
-    it 'should be empty' do
+   context 'when created' do 
+      let(:box) {Box.new}
+      it 'should be empty' do
         expect(box.empty?).to be true
+      end
+      it 'has nil inside' do
+        expect(box.stuff).to eq nil 
+      end
     end
+    # context 'when storing stuffs' do
+    #     it 'should add an object to the box' do
+    #         box.store([1,2,3,4])
+
+    #     end
+    # end
 end   
