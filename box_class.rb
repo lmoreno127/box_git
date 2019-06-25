f8ffd7b9c21c9450f2c99003b9f19c7dcb9bfb79
lmoreno127 <luismorenor127@hotmail.com>
2019-06-25 10:58:15 -0500

    defining Box Class

--- box_class.rb
+++ box_class.rb
@@ -1,5 +1,4 @@
-# 1. define a class named Box
-
+# 1. define a class named Box 
 # 2. all the new boxes should be created empty, use an instance variable to tell if the box is empty or not
 
 # 3. add a method to get the information of the instance variable you created in the last step
@@ -13,3 +12,5 @@
 # 7. if you try to store something else you should see a message telling: "the box is full"
 
 # 8. add a method to empty the box and update its status, if the box is already empty, show the message: "there is nothing in the box"
+class Box 
+end   
